6d497ceff1b2620eba51c9dc89bda70d43eb2333
v0l0v <vict0rl0pez@protonmail.com>
2026-05-13 21:16:58 +0200

    Mejorada la inyección: ahora interrumpe la música inmediatamente con un skip

--- scripts/inyectar_programa.sh
+++ scripts/inyectar_programa.sh
@@ -42,7 +42,11 @@ fi
 
 echo "Inyectando programa: $INTERNAL_PATH"
 
-# Comando vía Perl (mensajero telnet) para inyectar en la cola real
+# Comando vía Perl (mensajero telnet) para inyectar y saltar la canción actual
 docker exec liquidsoap perl -e 'use IO::Socket; my $s = IO::Socket::INET->new(PeerAddr => "localhost", PeerPort => 1234) or die "Socket error"; print $s "emergencia.push '"$INTERNAL_PATH"'\nexit\n"; close($s);'
 
-echo "✅ Inyección completada. Sonará al finalizar la canción actual."
+# Forzar el salto de la canción actual para que el programa entre YA
+# Enviamos skip a musica_normal para que Liquidsoap pase a la siguiente fuente de prioridad (emergencia)
+docker exec liquidsoap perl -e 'use IO::Socket; my $s = IO::Socket::INET->new(PeerAddr => "localhost", PeerPort => 1234) or die "Socket error"; print $s "musica_normal.skip\nexit\n"; close($s);'
+
+echo "✅ Inyección completada. El programa comienza INMEDIATAMENTE."
