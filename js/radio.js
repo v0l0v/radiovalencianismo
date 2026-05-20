6a56cc90ff411a83b949455650a4dc6904c8f08f
v0l0v <vict0rl0pez@protonmail.com>
2026-05-20 23:56:10 +0200

    Fix: Solucionar detalles restantes de Lighthouse (CLS, alt tags, websockets y caché)

--- js/radio.js
+++ js/radio.js
@@ -277,7 +277,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const LIGHTNING_ADDRESS = "boss@coinos.io";
     const NOSTR_PUBKEY = "72bdbc57bdd6dfc4e62685051de8041d148c3c68fe42bf301f71aa6cf53e52fb";
-    const RELAYS = ["wss://relay.coinos.io", "wss://relay.damus.io", "wss://nos.lol"];
+    const RELAYS = ["wss://relay.coinos.io", "wss://relay.damus.io", "wss://relay.primal.net"];
 
     const zapModal = document.getElementById("zap-modal");
     const zapQrImg = document.getElementById("zap-qr");
